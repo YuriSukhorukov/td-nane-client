372416e3d347e11031bbeee10d922c62ce3f5142
yurisukhorukov <yuri.sukhorukov.spb@gmail.com>
2021-05-01 20:40:44 +0300

    src: message component

---
+++ src/components/Chat/Message.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="wrapper">
+    {{created}} : {{sender}} : {{text}}
+    <!-- qwewqewqewqe {{created}} -->
+  </div>
+</template>
+
+<script setup>
+import {defineProps, computed} from 'vue';
+
+defineProps({
+  text: String, 
+  created: String,
+  sender: String,
+})
+</script>
+
+<style scoped>
+  .wrapper {
+    background-color: rgb(0, 183, 255)
+  }
+</style>
--- src/components/Chat/MessagesList.vue
+++ src/components/Chat/MessagesList.vue
@@ -1,7 +1,19 @@
 <template>
   <div class="wrapper">
     <el-container v-loading="isHistoryLoading" class="el-container-messages-list">
-      Messages
+      <!-- Messages -->
+      <div v-if="!isHistoryEmpty">
+        <!-- <Message v-for="message in messages" created="dssdsd" sender="sdsds" text="sdsds" /> -->
+        <!-- <div class="el-icon-chat-round"></div> -->
+        
+
+        <div v-if="!isHistoryEmpty">
+          <!-- <div v-for="message in messages"> -->
+            <Message v-for="message in messages" created="message.created" sender="message.sender" text="message.text" />
+            <!-- {{message.created}} : {{message.sender}} : {{message.text}} -->
+          <!-- </div> -->
+        </div>
+      </div>
     </el-container>
   </div>
 </template>
@@ -9,14 +21,19 @@
 <script setup>
 import {onMounted, computed} from 'vue';
 import {useStore} from 'vuex';
+import Message from './Message.vue';
 
 const store = useStore();
 
 const isHistoryEmpty = computed(() => {
-  return store.state.rooms.history == null && store.state.rooms.isHistoryLoading ;
+  return store.state.rooms.history == null && store.state.rooms.isHistoryLoading;
 });
 const isHistoryLoading = computed(() => {
   return store.state.rooms.isHistoryLoading;
+});
+const messages = computed(() => {
+  // console.log(store.state.rooms.history ? store.state.rooms.history[0].text : undefined);
+  return store.state.rooms.history ? store.state.rooms.history : null;
 });
 
 </script>
