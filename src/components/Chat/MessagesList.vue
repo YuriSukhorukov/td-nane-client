aed893a586fea4f975393af16446e3f8ae8221b8
yurisukhorukov <yuri.sukhorukov.spb@gmail.com>
2021-05-10 19:20:49 +0300

    edit: scroll after new message

--- src/components/Chat/MessagesList.vue
+++ src/components/Chat/MessagesList.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="wrapper">
+    <button @click="scrollToEndMessageList">Scroll</button>
     <el-container v-loading="isHistoryLoading" class="el-container-messages-list">
       <div v-if="!isHistoryEmpty">
         <div v-for="message in messages">
@@ -11,7 +12,7 @@
 </template>
 
 <script setup>
-import {onMounted, computed} from 'vue';
+import {onMounted, computed, watchEffect, watch, ref, reactive, nextTick} from 'vue';
 import {useStore} from 'vuex';
 import Message from './Message.vue';
 
@@ -26,6 +27,61 @@ const isHistoryLoading = computed(() => {
 const messages = computed(() => {
   return store.state.rooms.history && store.state.rooms.history[store.state.rooms.current] ? store.state.rooms.history[store.state.rooms.current] : null;
 });
+const scrollToEndMessageList = async () => {
+  var messages = document.getElementsByClassName("el-container-messages-list")[0].childNodes[1].childNodes;
+  var messagesLength = messages.length;
+  var messageLast = messages[messagesLength-2];
+  if (messageLast == undefined)
+    return;
+  if (messageLast.scrollIntoView == undefined)
+    return;
+  messageLast.scrollIntoView();
+};
+
+watch(messages, async (messages, prevMessages) => {
+  nextTick(scrollToEndMessageList);
+}, {deep: true});
+
+
+
+
+
+// const messagesWatch = watchEffect(()=>{
+//   console.log(messages);
+// }, {deep: true});
+
+// const s = reactive({messages: messages})
+
+// const selected = ref(store.state.rooms.history[store.state.rooms.current]);
+
+// watch(selected, (selection, prevSelection) => { 
+//    /* ... */ 
+//    console.log(selection)
+// })
+
+// watchEffect(()=>{
+//   console.log(s.length);
+// //   console.log(messages);
+// }, {deep: true});
+
+
+
+
+
+
+
+// store.watch((state)=>{state.rooms.history}, ()=>{
+//   console.log('!!!')
+// })
+// watch({
+//   messages: (value, oldValue) => {console.log('!!!')}
+// })
+
+// messagesWatch()
+
+// const sss = watch((messages)=>{
+//   console.log(messages);
+// })
 
 </script>
 
@@ -40,5 +96,7 @@ const messages = computed(() => {
 
   margin-left: 1%;
   /* margin-top: 1%; */
+
+  scroll-behavior: smooth;
 }
 </style>
